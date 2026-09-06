51de5d5e3a31ccff877ca26251b47be026f3bb7f
mazharmnzoor4227-beep <mazharmnzoor4227@gmail.com>
2026-09-06 18:46:54 +0500

    Update WhatsApp number in store configuration

--- store-config.js
+++ store-config.js
@@ -2,9 +2,7 @@ window.STORE_CONFIG = {
   storeName: "Clawora Beauty",
   currency: "PKR",
 
-  // WhatsApp number country code के साथ, बिना + के
-  // Example: 923422271226
-  whatsappNumber: "",
+  whatsappNumber: "923422271226",
 
   supportEmail: "",
   instagram: "",
